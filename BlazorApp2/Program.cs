6acfa5e0c10e3e0449f50025431cce2b3a43547c
Pedro Rômulo <67558160+Oloro7965@users.noreply.github.com>
2024-10-14 14:22:14 -0300

    ajustes para integração com API e renderização

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -1,15 +1,33 @@
 using BlazorApp2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http.Json;
+using MudBlazor;
+using BlazorApp2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp =>
-    new HttpClient
-    {
-        BaseAddress = new Uri("https://localhost:7154")
-    });
+var httpClient= new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var appSettings = await httpClient.GetFromJsonAsync<AppSettings>("appsettings.json");
+var baseurl = appSettings!.baseurls.ApiRecur;
+builder.Services.AddScoped(sp=>new HttpClient { BaseAddress=new Uri(baseurl) });
+
+    //builder.Services.AddScoped(sp =>
+    //   new HttpClient
+    //   {
+    //        BaseAddress = new Uri("https://localhost:7154")
+    //    });
 await builder.Build().RunAsync();
+
+
+
+public class AppSettings
+{
+    public BaseUrls baseurls { get; set; }
+}
+public class BaseUrls
+{
+    public string ApiRecur { get; set; }
+}
